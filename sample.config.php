2199ce54542deb79234a76cd4398439ac32d76f1
Russ Posluszny <russjp1985@gmail.com>
2015-09-13 11:51:47 -0400

    Test config

---
+++ sample.config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('APP_ROOT', dirname(__FILE__));
+define('WWW_ROOT', APP_ROOT.'/www');
+define('PHP_ROOT', APP_ROOT.'/sample_lib');
+define('CORE_ROOT', APP_ROOT.'/phplib');
+
+$CONFIG = [
+
+
+];
--- sample.router.php
+++ sample.router.php
@@ -14,13 +14,8 @@
  *    // Git-submodule to this project
  */
 
-define('WWW_ROOT', dirname(__FILE__));
-define('APP_ROOT', dirname(WWW_ROOT));
-define('PHP_ROOT', APP_ROOT.'/phplib');
-define('CORE_ROOT', APP_ROOT.'/Core');
-define('CONFIG_PATH', APP_ROOT.'/development.php');
-
-require APP_ROOT.'/routes.php';
+require dirname(__FILE__).'/sample.config.php';
 require CORE_ROOT.'/loader.php';
 
+require APP_ROOT.'/routes.php';
 Router::route($routes);
